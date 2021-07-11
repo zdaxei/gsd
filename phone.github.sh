290d9a852f2ee9597307ade32d1014f1e27bbb5e
zdaxei <351637455@qq.com>
2021-07-11 21:40:24 +0800

    new phone number

--- phone.github.sh
+++ phone.github.sh
@@ -232,8 +232,8 @@ myEcho "开始第 ${numI_next} 次操作"
 
 phoneN="./phone.txt"
 [[ ! -f ${phoneN} ]] && (echo 0 >${phoneN})
-left=17
-center=1
+left=14
+center=9
 start=$(cat "${phoneN}")
 if [[ "${start}" == "10000" ]]; then
     myEcho "号码【${left}${center}（0000 ---> 9999）】处理完毕"
