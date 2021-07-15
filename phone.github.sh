31c24f77b67c7b531fa04a43709f6b4ed2c1f0e5
zdaxei <351637455@qq.com>
2021-07-15 17:25:07 +0800

    fix

--- phone.github.sh
+++ phone.github.sh
@@ -51,8 +51,8 @@ doGsd () {
     while ( [ ! -f "${phoneD}" ] && [ ${i} -lt ${phoneNumLimit} ] ); do
         ((i+=1))
         curl -s -o "${phoneD}" -k -G -d "op=insert" -d "val=%7B%22tel%22:%22${phone}%22,%22gsd%22:%22${1}%22%7D" "${API_URL}"
-        myEcho "GSD insert 第 ${i} 次 等待 1 秒"
-        sleep 1
+        myEcho "GSD insert 第 ${i} 次 等待 3 秒"
+        sleep 3
     done
     strDone=$(cat "${phoneD}")
     rm -f "${phoneD}"
@@ -73,15 +73,15 @@ curlCHB () {
             -H "Referer: ${API_URL_CHB}%E9%A6%96%E9%A1%B5" \
             -s -o "${phoneP}" -k \
             "${API_URL_CHB}${phone}?${cur_sec}"
-        myEcho "CHB ${phone}?${cur_sec} 第 ${i} 次 等待 1 秒"
-        sleep 1
+        myEcho "CHB ${phone}?${cur_sec} 第 ${i} 次 等待 3 秒"
+        sleep 3
         if [[ ! -f "${phoneP}" ]]; then
             if [[ ${i} = 3 ]]; then
                 echo >${phoneP}
                 myEcho "CHB ${phone}?${cur_sec} 第 ${i} 次 跳过号码【${phone}】"
             else
-                chbMin=`awk -v x=0.1 -v y=60 'BEGIN{printf "%.0f\n",x*y}'`
-                chbMax=`awk -v x=0.3 -v y=60 'BEGIN{printf "%.0f\n",x*y}'`
+                chbMin=`awk -v x=0.2 -v y=60 'BEGIN{printf "%.0f\n",x*y}'`
+                chbMax=`awk -v x=0.4 -v y=60 'BEGIN{printf "%.0f\n",x*y}'`
                 chbRand=$[$RANDOM%$((${chbMax}-${chbMin}+1))+${chbMin}]
                 chbNext=$(date --date="${chbRand} second" '+%Y-%m-%d %H:%M:%S')
                 myEcho "CHB ${phone}?${cur_sec} 第 ${i} 次 未获取到 等待 ${chbRand} 秒 下次操作: ${chbNext}"
@@ -213,8 +213,8 @@ curlPhone () {
     while ( [ ! -f "${phoneG}" ] && [ ${i} -lt ${phoneNumLimit} ] ); do
         ((i+=1))
         curl -s -o "${phoneG}" -k -G -d "op=getOne" -d "tel=${phone}" "${API_URL}"
-        myEcho "GSD getOne 第 ${i} 次 等待 1 秒"
-        sleep 1
+        myEcho "GSD getOne 第 ${i} 次 等待 3 秒"
+        sleep 3
     done
 }
 
@@ -259,8 +259,8 @@ curlPhone
 jsonG=$(cat "${phoneG}")
 myEcho "查询号码 【${phone}】，返回信息 ${jsonG}"
 if [[ -z "${jsonG}" ]]; then
-    myEcho "查询失败，1 秒后重新查询"
-    sleep 1
+    myEcho "查询失败，3 秒后重新查询"
+    sleep 3
     curlPhone
     jsonG=$(cat "${phoneG}")
     myEcho "查询号码 【${phone}】，返回信息 ${jsonG}"
@@ -276,8 +276,8 @@ rm -f "${phoneG}"
 if [[ ${goonNext} -ne 1 ]]; then
     echo ${numI_next} >${phoneI}
     if [[ ${numI_next} -lt ${phoneNL} ]]; then
-        numMin=`awk -v x=0.1 -v y=60 'BEGIN{printf "%.0f\n",x*y}'`
-        numMax=`awk -v x=0.3 -v y=60 'BEGIN{printf "%.0f\n",x*y}'`
+        numMin=`awk -v x=0.2 -v y=60 'BEGIN{printf "%.0f\n",x*y}'`
+        numMax=`awk -v x=0.4 -v y=60 'BEGIN{printf "%.0f\n",x*y}'`
         numRand=$[$RANDOM%$((${numMax}-${numMin}+1))+${numMin}]
         timeNext=$(date --date="${numRand} second" '+%Y-%m-%d %H:%M:%S')
         myEcho "下次操作: ${timeNext}"
